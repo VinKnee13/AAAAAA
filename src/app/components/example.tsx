2109f3e8bb5b7e5c636367d22653e5be22e9958b
Lucas Griffin <66416659+LucasGriffinDev@users.noreply.github.com>
2024-08-03 20:08:36 +1000

    Create example.tsx

---
+++ src/app/components/example.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+const Example = () => {
+    return (
+        <>
+            <div className="w-full h-20 bg-emerald-200 sticky top-0">
+                <h1>
+                    example reuseable component
+                </h1>
+                <h1>
+                    test branch
+                </h1>
+                
+            </div>
+        </>
+    );
+};
+
+export default Example;

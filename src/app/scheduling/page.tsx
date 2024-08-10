3ae7144d68aa5c54176619571cbc5608305e4335
Luca <103352762@student.swin.edu.au>
2024-08-10 17:22:46 +1000

    Added buttons to add, edit and remove inventory items

--- src/app/scheduling/page.tsx
+++ src/app/scheduling/page.tsx
@@ -5,6 +5,8 @@ export default function facilityManagment() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-around p-24">
             <h1 className="text-4xl">Scheduling Management Page</h1>
+
+           
         </main>
     );
 }
